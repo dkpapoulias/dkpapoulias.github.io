0370e1f3e2e371f727b396fecefb25eb6160c888
dkpapoulias <dk.papoulias@gmail.com>
2026-03-08 20:50:36 +0000

    Deploying to gh-pages from @ dkpapoulias/dkpapoulias.github.io@fca2bdf8212ece2858d03477e5664a0663e227d5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Reach out for a detailed CV in pdf",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
