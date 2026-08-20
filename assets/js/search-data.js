2791fac6618c19aae69970f34b47d085d90c00eb
dkpapoulias <dk.papoulias@gmail.com>
2026-08-20 11:15:13 +0000

    Deploying to gh-pages from @ dkpapoulias/dkpapoulias.github.io@51c8f938db9cae78fac651cc7f31bb4932f331e7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-research",
+          title: "Research",
+          description: "Research interests and selected contributions",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
         },{id: "nav-neutrinosphere-eu-project",
           title: "neutrinoSPHERE (EU project)",
           description: "",
